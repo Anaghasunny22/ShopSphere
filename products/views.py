ade6ae4a90664d60d0779b0a79861c4b3d2fb5cc
Anagha Sunny <anaghasunny22@gmail.com>
2026-01-13 13:13:18 +0530

    Second commit: updates after first push

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def list_products(request):
+    """_summary_
+
+    return product list page
+
+    Docstring for list_products
+    
+    :param request: Description
+    """
+    return render(request,'products.html')
+
+def detail_products(request):
+    return render(request,'product_detail.html')
+
+
